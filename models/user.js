3edbbd9557c9286abe3a1d07a2984a6657307deb
davidmgudeman <davidmgudeman@gmail.com>
2017-06-13 11:56:28 -0700

    fixed the password hashing with saving asynchronously by returning a promise

--- models/user.js
+++ models/user.js
@@ -4,48 +4,48 @@ const bcrypt = require('bcrypt');
 
 const saltRounds = 10;
 
-module.exports = function (sequelize, DataTypes) {
-  const User = sequelize.define('User', {
-    id: {
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    firstname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    username: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-    // start options ---------------------------------------
-    {
-      freezeTableName: true,
-      tableName: 'user',
-      dialect: 'mysql',
-  
-      hooks: {
-         beforeCreate: function (user, options) {
-             const myPlaintextPassword = 'user.password';
-             var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-             return    user.password = hash;
-         },
-      } // end hooks
-    }  // end options
-  );
-  return User;
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define('User', {
+            id: {
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            firstname: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            lastname: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            username: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        // start options ---------------------------------------
+        {
+            freezeTableName: true,
+            tableName: 'user',
+            dialect: 'mysql',
+            hooks: {
+                beforeCreate: function(user, options) {
+                    return new Promise((resolve, reject) => {
+                        bcrypt.hash(user.password, saltRounds).then(function(hash) {
+                              user.password = hash;
+                              resolve(user.password);
+                              reject(console.log('Error is hashing password'));
+                        })
+                    });
+                } // end beforeCreate
+            } // end hooks
+        } // end options
+    );
+    return User;
 };
-      
-      
-     
--- routes/user.js
+++ routes/user.js
@@ -5,21 +5,20 @@ const User = require('../models/').User;
 
 router.post('/', function(req, res, next) {
     let user = req.body;
-    console.log(`user route BE req.body= ${JSON.stringify(req.body)}`);
-    User
 
-    .create(user)
-    .then((user) => {
-    console.log(`register ${JSON.stringify(user)}`);
+    
+    User.create(user)
+        .then((user) => {
+        console.log(`register ${JSON.stringify(user)}`);
 
-       return res.status(201).json({
-            user,
-        });
-    }).catch((error) => {
-        console.log(error.stack);
-    return res.status(400).json({
-            error: error.stack,
-        });
+        return res.status(201).json({
+                user,
+            });
+        }).catch((error) => {
+            console.log(error.stack);
+            return res.status(400).json({
+                error: error.stack,
+            });
     });
 });
 
