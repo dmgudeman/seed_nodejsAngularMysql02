e6dfd1f967158b50b7ae07ba0868b950478a67b6
davidmgudeman <davidmgudeman@gmail.com>
2017-06-15 08:28:50 -0700

    cleaned up code and did some work on error hangling. The jwt is built appropriately on the back end.

--- routes/user.js
+++ routes/user.js
@@ -4,17 +4,14 @@ var router = express.Router();
 const User = require('../models/').User;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+// -----------------------------------------------------------
 router.post('/', function(req, res, next) {
     let user = req.body;
     
     User.create(user)
         .then((user) => {
-        console.log(`register ${JSON.stringify(user)}`);
-         
-        return res.status(201).json({
-                user,
-            });
+            // console.log(`register ${JSON.stringify(user)}`);
+            return res.status(201).json({ user, });
         })
         .catch((error) => {
             console.log(error.stack);
@@ -24,39 +21,46 @@ router.post('/', function(req, res, next) {
     });
 });
 
+// -----------------------------------------------------------
+
 router.post('/login', function(req, res, next){
     let liUser = req.body;
     let user;
-        User.findOne({where: {username: liUser.username}})
-           .then(userr => {
-               console.log(`USERRRRRRR ${userr}`);
-               user = userr;
-              return user;
-           })
-           .then((user) => {
-              let flag =bcrypt.compare(liUser.password, user.password)
-                return flag;
-           })
+    User.findOne({where: {username: liUser.username}})
+        .then(data => {
+            if (data) {
+                user = data;
+                return user;
+            } else {
+                return console.log ('user not found');
+            }
+        })
+        .then((user) => {
+            let flag =bcrypt.compare(liUser.password, user.password)
+            if (flag) {
+                 return flag;
+            } else {
+                return console.log( 'passwords did not match');
+            }
+        })
         .then((flag) => {
-        console.log(`USER IN JWT ${user}`);
-        if(flag){
-        const token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
-        const response = { message: 'Successfully logged in',
-                           token: token,
-                           userId: user.id}
-            
-                  res.status(201).json(response)
-              } else {
-                  console.log('third method failed')
-              }
-        // });
-    })
-    .catch((error) => {
-            console.log(error.stack);
-            return res.status(400).json({
-                error: error.stack,
-            });
-    });
+            if (flag) {
+                const token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+                const response = { 
+                    message: 'Successfully logged in',
+                    token: token,
+                    userId: user.id}
+                    res.status(201).json(response)
+            } else {
+                return console.log('failed to login')
+            }
+        })
+        .catch((error) => {
+                console.log(error.stack);
+                return res.status(400).json({
+                    error: error.stack,
+                });
+        });
 });
    
 module.exports = router;
