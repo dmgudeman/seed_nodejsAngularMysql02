8ebb455c8c6284da89089b1436d2163d4d19c149
davidmgudeman <davidmgudeman@gmail.com>
2017-06-21 08:18:32 -0700

    pasted a copy of backend-better-invoice into models folder by mistake. Removed everything but may have modified the index.js. Doubt it.

--- models/index.js
+++ models/index.js
@@ -7,11 +7,11 @@ var env       = process.env.NODE_ENV || "development";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 
 if (process.env.DATABASE_URL) {
-  var sequelize = new Sequelize(process.env.DATABASE_URL,config);
+  var sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-var db        = {};
+const db = {};
 
 fs
   .readdirSync(__dirname)
